efe72f0dd4d4805c57a61e17eeda370f21554c27
Ana <anatran334@gmail.com>
2023-05-28 18:55:11 -0400

    finalized hero.js
    added resume and pictures in assets
    finalized hero component

--- portfolio-website/src/components/headerNavBar.js
+++ portfolio-website/src/components/headerNavBar.js
@@ -3,15 +3,31 @@ import React from "react";
 
 export default function headerNavBar() {
   return (
-<div class="container">
-    <header class="d-flex justify-content-center py-3">
-      <ul class="nav nav-pills">
-        <li class="nav-item"><a href="#home" class="nav-link " aria-current="page">Home</a></li>
-        <li class="nav-item"><a href="#projects" class="nav-link">Projects</a></li>
-        <li class="nav-item"><a href="#about" class="nav-link">About Me</a></li>
-        <li class="nav-item"><a href="#contact" class="nav-link">Contact</a></li>
-      </ul>
-    </header>
-  </div>
+    <div class="container">
+      <header class="d-flex justify-content-center py-3">
+        <ul class="nav nav-pills">
+          <li class="nav-item">
+            <a href="#home" class="nav-link " aria-current="page">
+              Home
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="#projects" class="nav-link">
+              Projects
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="#about" class="nav-link">
+              About Me
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="#contact" class="nav-link">
+              Contact
+            </a>
+          </li>
+        </ul>
+      </header>
+    </div>
   );
 }
--- portfolio-website/src/components/hero.js
+++ portfolio-website/src/components/hero.js
@@ -1,29 +1,53 @@
 import React from "react";
+import logo from "../assets/logo.png";
+import resume from "../resources/Ana_Tran_Resume.pdf";
+import { TypeAnimation } from "react-type-animation";
 
 export default function hero() {
+  const downloadResume = () => {
+    window.open(resume, "_blank");
+  };
+
   return (
     <div class="px-4 py-5 my-5 text-center">
       <img
         class="d-block mx-auto mb-4"
-        src="/docs/5.3/assets/brand/bootstrap-logo.svg"
-        alt=""
-        width="72"
-        height="57"
+        src={logo}
+        alt="pom pom purin eating"
+        width="150"
+        height="150"
       />
-      <h1 class="display-5 fw-bold text-body-emphasis">Centered hero</h1>
+      <h1 class="display-5 fw-bold text-body-emphasis">Ana Tran</h1>
+      <h4 class="display-10">
+        <TypeAnimation
+          sequence={[
+            "I am a problem-solver",
+            900,
+            "I am a developer",
+            900,
+            "I am a gamer",
+            1300,
+          ]}
+          speed={30}
+          repeat={Infinity}
+        />
+      </h4>
       <div class="col-lg-6 mx-auto">
         <p class="lead mb-4">
-          Quickly design and customize responsive mobile-first sites with
-          Bootstrap, the world’s most popular front-end open source toolkit,
-          featuring Sass variables and mixins, responsive grid system, extensive
-          prebuilt components, and powerful JavaScript plugins.
+          Hi, my name is Ana and I am a new graduate looking for software
+          developer roles. I am a driven and passionate individual who is eager
+          to begin my career. I am currently learning react, python, and
+          flutter. I am excited to bring my skills and enthusiasm to a new role
+          and look forward to the opportunity to grow and learn in a dynamic
+          work environment.
         </p>
         <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
-          <button type="button" class="btn btn-primary btn-lg px-4 gap-3">
-            Primary button
-          </button>
-          <button type="button" class="btn btn-outline-secondary btn-lg px-4">
-            Secondary
+          <button
+            type="button"
+            class="btn btn-primary btn-lg px-4 gap-3"
+            onClick={downloadResume}
+          >
+            Resume
           </button>
         </div>
       </div>
