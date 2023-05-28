bf98a71e3fa6e1ff48f693ea673f3a8c60c372f2
Ana <anatran334@gmail.com>
2023-05-28 19:13:44 -0400

    finalized footer
    finalized footer
    added bootstrap svg icons and packages

--- portfolio-website/src/components/HomePage.js
+++ portfolio-website/src/components/HomePage.js
@@ -1,10 +1,11 @@
 import React from "react";
-import { Hero, Navbar } from ".";
+import { Footer, Hero, Navbar } from ".";
 export default function HomePage() {
 return (
     <div className="container">
       <Navbar/>
       <Hero/>
+      <Footer/>
     </div>
 );
 }
--- portfolio-website/src/components/index.js
+++ portfolio-website/src/components/index.js
@@ -1,2 +1,3 @@
 export { default as Navbar } from './headerNavBar'
 export {default as Hero } from './hero'
+export {default as Footer} from './footer'
