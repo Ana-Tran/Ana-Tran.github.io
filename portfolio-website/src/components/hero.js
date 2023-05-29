ad8481141c5d53a39fd51d6d6b1f3f77b7ae51a8
Ana <anatran334@gmail.com>
2023-05-29 18:30:50 -0400

    Update hero.js
    adjusted p text

--- portfolio-website/src/components/hero.js
+++ portfolio-website/src/components/hero.js
@@ -34,7 +34,7 @@ export default function hero() {
       </h4>
       <div class="col-lg-6 mx-auto">
         <p class="lead mb-2">
-          Click the button view my resume
+          To view my resume, click the button.
         </p>
         <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
           <button
