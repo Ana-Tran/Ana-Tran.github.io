9919fc914ee572261b18ebfe67a1c7d2ac1cd9bf
Ana <anatran334@gmail.com>
2023-06-03 14:46:10 -0400

    Update headerNavBar.js
    added box shadow to navbar

--- portfolio-website/src/components/headerNavBar.js
+++ portfolio-website/src/components/headerNavBar.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function headerNavBar() {
   return (
     <header class="d-flex justify-content-center py-3">
-      <nav class="navbar fixed-top navbar-light bg-light">
+      <nav class="navbar fixed-top navbar-light bg-light shadow-sm p-3 mb-5 bg-white rounded">
         <ul class="nav nav-pills">
           <li class="nav-item">
             <a class="nav-link" href="#home">
