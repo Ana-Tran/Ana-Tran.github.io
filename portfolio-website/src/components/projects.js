65a6693e4173077316eccb3b662856d0cd66d231
Ana <anatran334@gmail.com>
2023-06-02 23:49:30 -0400

    Added animation
    added framer motion package
    added animation to project.js for hover and tap

--- portfolio-website/src/components/projects.js
+++ portfolio-website/src/components/projects.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { motion } from "framer-motion";
 
 export default function projects() {
   return (
@@ -6,7 +7,10 @@ export default function projects() {
     <h2 class="pb-2 border-bottom">Projects</h2>
 
     <div class="row row-cols-1 row-cols-lg-3 align-items-stretch g-4 py-5">
-      <div class="col">
+      <motion.div class="col"
+        whileHover={{ scale: 1.1 }}
+        whileTap={{ scale: 0.9 }}
+      >
         <div class="card card-cover h-100 overflow-hidden text-bg-dark rounded-4 shadow-lg">
           <div class="d-flex flex-column h-100 p-5 pb-3 text-white text-shadow-1">
             <h3 class="pt-5 mt-5 mb-4 display-6 lh-1 fw-bold">Short title, long jacket</h3>
@@ -25,9 +29,12 @@ export default function projects() {
             </ul>
           </div>
         </div>
-      </div>
+      </motion.div>
 
-      <div class="col">
+      <motion.div class="col"
+      whileHover={{ scale: 1.1 }}
+      whileTap={{ scale: 0.9 }}
+      >
         <div class="card card-cover h-100 overflow-hidden text-bg-dark rounded-4 shadow-lg">
           <div class="d-flex flex-column h-100 p-5 pb-3 text-white text-shadow-1">
             <h3 class="pt-5 mt-5 mb-4 display-6 lh-1 fw-bold">Much longer title that wraps to multiple lines</h3>
@@ -46,9 +53,12 @@ export default function projects() {
             </ul>
           </div>
         </div>
-      </div>
+      </motion.div>
 
-      <div class="col">
+      <motion.div class="col"
+      whileHover={{ scale: 1.1 }}
+      whileTap={{ scale: 0.9 }}
+      >
         <div class="card card-cover h-100 overflow-hidden text-bg-dark rounded-4 shadow-lg">
           <div class="d-flex flex-column h-100 p-5 pb-3 text-shadow-1">
             <h3 class="pt-5 mt-5 mb-4 display-6 lh-1 fw-bold">Another longer title belongs here</h3>
@@ -67,7 +77,7 @@ export default function projects() {
             </ul>
           </div>
         </div>
-      </div>
+      </motion.div>
     </div>
   </div>
 );
